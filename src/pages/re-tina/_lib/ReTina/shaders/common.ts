23c36aa5c58914449281eb880337e8ca4ccbb578
moloxe <>
2025-09-04 21:43:53 -0500

    feat: rgb2hsv wgsl function

--- src/pages/re-tina/_lib/ReTina/shaders/common.ts
+++ src/pages/re-tina/_lib/ReTina/shaders/common.ts
@@ -50,6 +50,15 @@ fn hsv2rgb(c: vec3f) -> vec3<f32> {
     return c.z * mix(K.xxx, clamp(p - K.xxx, vec3<f32>(0.0), vec3<f32>(1.0)), c.y);
 }
 
+fn rgb2hsv(c: vec3f) -> vec3<f32> {
+    let K = vec4<f32>(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    let p = mix(vec4<f32>(c.zy, K.wz), vec4<f32>(c.yz, K.xy), step(c.b, c.g));
+    let q = mix(vec4<f32>(p.xyw, c.r), vec4<f32>(c.r, p.yzx), step(p.x, c.r));
+    let d = q.x - min(q.w, q.y);
+    let e = 1.0e-10;
+    return vec3<f32>(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
 // https://iquilezles.org/articles/distfunctions/ 💪
 
 fn sdSphere(p: vec3f, s: f32) -> f32 {
