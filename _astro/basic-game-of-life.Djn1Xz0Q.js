6ef8f0f26ea297291119ed35ddd63b6830ce0d0e
moloxe <23423636+moloxe@users.noreply.github.com>
2025-12-28 16:25:32 -0500

    Deploying to gh-pages

--- _astro/basic-game-of-life.Djn1Xz0Q.js
+++ _astro/basic-game-of-life.Djn1Xz0Q.js
@@ -1,4 +1,4 @@
-import{R as h}from"./ReTina.DUZmNRbc.js";const l={src:"/_astro/tina.4YsElRAf.jpeg"};async function g(s){const e=document.createElement("img");e.src=s,await e.decode();const t=document.createElement("canvas");t.width=e.width,t.height=e.height;const n=t.getContext("2d");if(!n)throw new Error("No canvas context found");n.drawImage(e,0,0,t.width,t.height);const u=n.getImageData(0,0,t.width,t.height),o=new Uint8Array(e.width*e.height*4);for(let i=0;i<e.width*e.height*4;i++)o[i]=u.data[i];return{textureData:o,width:e.width,height:e.height}}const a=await g(l.src),r=new h({fps:24,height:128,texs:[{width:a.width,height:a.height}],usePrevFrameTex:!0,functions:`
+import{R as h}from"./ReTina.CNHRwvP8.js";const l={src:"/_astro/tina.4YsElRAf.jpeg"};async function f(s){const e=document.createElement("img");e.src=s,await e.decode();const t=document.createElement("canvas");t.width=e.width,t.height=e.height;const n=t.getContext("2d");if(!n)throw new Error("No canvas context found");n.drawImage(e,0,0,t.width,t.height);const u=n.getImageData(0,0,t.width,t.height),a=new Uint8Array(e.width*e.height*4);for(let i=0;i<e.width*e.height*4;i++)a[i]=u.data[i];return{textureData:a,width:e.width,height:e.height}}const o=await f(l.src),r=new h({fps:24,height:128,texs:[{width:o.width,height:o.height}],usePrevFrameTex:!0,functions:`
     fn isPixAlive(pos: vec2f) -> vec3f {
       return step(vec3f(0.5), getPrevFrameTexSample(pos).rgb);
     }
@@ -15,8 +15,8 @@ import{R as h}from"./ReTina.DUZmNRbc.js";const l={src:"/_astro/tina.4YsElRAf.jpe
       return 0f;
     }
   `,main:`
-    if (U.time < 1) {
-      return getTex0Sample(uv).bgra; // TODO: bgra?
+    if (U.time < 1 || U.frame < 60) {
+      return getTex0Sample(uv);
     } else {
       let res = vec2f(U.width, U.height);
       var nCount = vec3f(0);
@@ -37,4 +37,4 @@ import{R as h}from"./ReTina.DUZmNRbc.js";const l={src:"/_astro/tina.4YsElRAf.jpe
       );
       return vec4f(newState, 1);
     }
-  `});r.start({onBuild(){r.setTex(0,a.textureData)}});
+  `});r.start({onBuild(){r.setTex(0,o.textureData)}});
